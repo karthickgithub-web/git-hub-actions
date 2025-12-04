8c74f374ae3615bef6e4881a4507608d4f929e6a
Karthick Anburajan <110174311+karthickgithub-web@users.noreply.github.com>
2025-12-04 17:29:31 +0530

    Delete terminate-s3-bucket.tf

--- terminate-s3-bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-jishnu-hardik-managed-bucket-2025"
-
-  # Setting this to true allows Terraform to empty the bucket contents
-  # before deleting the bucket on 'terraform destroy'.
-  force_destroy = true 
-
-  # ... other bucket config (versioning, public access block, etc.)
-}
