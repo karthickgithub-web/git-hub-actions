7ca7e3f1e1a6ffe2e652d9c16d1432f5fece8667
Karthick Anburajan <110174311+karthickgithub-web@users.noreply.github.com>
2025-12-04 16:27:16 +0530

    Create AWS Security Group for web server

---
+++ sg.tf
@@ -0,0 +1,36 @@
+# Create the Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-security-group"
+  description = "Allow HTTP and SSH inbound traffic"
+  vpc_id      = vpc-0ffde819dac5b7639
+
+  # Ingress Rule for SSH (Port 22)
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/0"]
+  }
+
+  # Ingress Rule for HTTP (Port 80)
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/0"]
+  }
+
+  # Egress Rule (All outbound traffic allowed)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 means all protocols
+    cidr_blocks = ["10.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web_Security_Group"
+  }
+}
